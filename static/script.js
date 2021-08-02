4a5b7f25549e196290c0a324e0756a71bb94a058
unknown <cjmugs@gmail.com>
2021-08-02 16:09:46 -0500

    sdd

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 
 function myFunction() {
+    alert('Are you sure you want to run THIS program?  Click Ok')
     document.getElementById("button").innerHTML = "RUNNING PROGRAM . . . .!";}
 
 console.log('Hello and thanks for accessing me . . .but sadly I am just a server')
