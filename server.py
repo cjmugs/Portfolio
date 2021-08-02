2dc9f2edcdf43e8d8c8e5eee9c4894087a5d9a7f
unknown <cjmugs@gmail.com>
2021-08-02 16:24:12 -0500

    sdd

--- server.py
+++ server.py
@@ -9,6 +9,18 @@ print(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route("/index.html")                       
+def home():
+    return ('Home Homie')
+
+@app.route("/about.html")                       
+def about():
+    return ('Thats what Im about')
+
+@app.route("/projects.html")                       
+def projects():
+    return ('Projecting Projects')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
