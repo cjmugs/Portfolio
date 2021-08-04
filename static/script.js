c60faaafe7443a16c3bc9149b1dad27817a7d74b
unknown <cjmugs@gmail.com>
2021-08-04 16:20:03 -0500

    df

--- static/script.js
+++ static/script.js
@@ -3,6 +3,3 @@ function myFunction() {
     alert('Are you sure you want to run this test program?  Click Ok')
     document.getElementById("button").innerHTML = alert('program is running fine');
 }
-
-
-console.log('Hello and thanks for accessing me . . .but sadly I am just a server')
