d8aa22dab12653cbd38d284352a0fc389c79be00
unknown <cjmugs@gmail.com>
2021-08-04 21:59:07 -0500

    sd

--- static/script.js
+++ static/script.js
@@ -4,4 +4,6 @@ function myFunction() {
     document.getElementById("button").innerHTML = alert('program is running fine');
     document.getElementById("button").innerHTML = alert('no seriously its working');
     document.getElementById("button").innerHTML = alert('oh no nevermind its crashing now . . . ');
+    var tag_id = document.getElementById('button');
+    tag_id.innerHTML = 'ShitShitShitShitShitShitShitShitShitShitShitShitShitShitShitShitShit';
 }
