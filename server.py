e8d49068d5033c2e121d7550b3316972175de65b
unknown <cjmugs@gmail.com>
2021-08-11 20:13:58 -0500

    df

--- server.py
+++ server.py
@@ -21,11 +21,9 @@ def projects():
 def contact():
     return render_template('contact.html')
 
-@app.route('/<string:page_name>')                   
-def html_page(page_name):
-    return render_template(page_name)
 
-@app.route("/submit_form", methods=['POST', 'GET'])
+
+@app.route("/data.html", methods=['POST', 'GET'])
 def data():
     if request.method == 'POST':
        data = request.form.to_dict()
